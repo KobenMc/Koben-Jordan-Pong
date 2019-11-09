f3837efbf741892f179b4564d5837311f3878545
Koben McClellan <koben.mcclellan@digipen.edu>
2019-11-08 18:03:45 -0800

    Added border and smooth movement to ball script

--- Pong/Assets/Scripts/BallMovement.cs
+++ Pong/Assets/Scripts/BallMovement.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+class RotationMatrix2D
+{
+    public RotationMatrix2D(float Angle)
+    {
+        mMatrix = new float[2, 2];
+        float sinTheta = Mathf.Sin(Angle);
+        float cosTheta = Mathf.Cos(Angle);
+        mMatrix[0, 0] = cosTheta;
+        mMatrix[0, 1] = -sinTheta;
+        mMatrix[1, 0] = sinTheta;
+        mMatrix[1, 1] = cosTheta;
+    }
+}
 public class BallMovement : MonoBehaviour
 {
-    [SerializeField]
-    float forceValue = 5.0f;
-
+    [SerializeField] private float BallSpeed = 10.0f;
     Rigidbody2D myBody;
     // Use this for initialization
     void Start()
     {
         myBody = GetComponent<Rigidbody2D>();
-        myBody.AddForce(new Vector2(forceValue * 50, 50));
+        float launchAngle = Random.Range(-50, 50);
+        Vector2 launchVec = new Vector2(1, 0);
+        float goesLeft = Random.Range(-1, 1);
+        if (goesLeft > 0.0f)
+        {
+            launchAngle += 180.0f;
+            launchVec = Math.ApplyRotationTo(launchVec, launchAngle);
+            myBody.velocity = launchVec; 
+        }
     }
 
     // Update is called once per frame
@@ -22,9 +41,13 @@ public class BallMovement : MonoBehaviour
     }
     void BallBounce()
     {
-        if ()
+        if (transform.position.y > 3.45f)
         {
-
+            myBody.velocity = new Vector2(myBody.velocity.x, -myBody.velocity.y);
+        }
+        if (transform.position.y > -3.45f)
+        {
+            myBody.velocity = new Vector2(myBody.velocity.x, -myBody.velocity.y);
         }
     }
 }
