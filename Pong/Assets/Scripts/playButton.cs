90164ac5a7362fd7a60f51aac9057e057bdcd87d
jordanluma <jordan.luma@digipen.edu>
2019-11-08 18:01:53 -0800

    commit

---
+++ Pong/Assets/Scripts/playButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class playButton : MonoBehaviour
+{
+    void OnMouseUp()
+    {
+        SceneManager.LoadScene("FinalScene", LoadSceneMode.Single);
+    }
+
+}
+
